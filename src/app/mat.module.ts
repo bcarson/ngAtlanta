5d67624b054ef5a0a2e913d7877a3fab7518969e
samlee13 <samantha_lee_509@yahoo.com>
2018-01-13 14:32:09 -0600

    Cars Component

--- src/app/mat.module.ts
+++ src/app/mat.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-    imports: [
-        MatCheckboxModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatTabsModule
-    ],
-    exports: [
-        MatCheckboxModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatTabsModule
-    ]
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatSelectModule
+  ]
 })
-
-export class MatModule { }
+export class MatModule {}
