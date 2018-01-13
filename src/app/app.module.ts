d688995c3e924a6350c46e5a863d1e80dc80ba2a
Bonnie Brennan <bjcarson75@yahoo.com>
2018-01-13 11:46:30 -0600

    Roughed out cars form, added animation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatModule } from './mat.module';
@@ -15,6 +16,7 @@ import { FlightsComponent } from './flights/flights.component';
     FlightsComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     MatModule,
     ReactiveFormsModule
