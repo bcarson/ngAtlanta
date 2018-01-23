31167bfebe9618fc1e27b78db02fb5dd20a4f40e
samlee13 <samantha_lee_509@yahoo.com>
2018-01-23 14:35:43 -0600

    Adding flights

--- src/app/shared/constants/airports.ts
+++ src/app/shared/constants/airports.ts
@@ -16,5 +16,23 @@ export const airports = [
     code: 'DFW',
     city: 'Dallas',
     name: 'Dallas Fort Worth Airport'
+  },
+  {
+    id: 3,
+    code: 'STL',
+    city: 'St. Louis',
+    name: 'St. Louis Lambert International Airport'
+  },
+  {
+    id: 4,
+    code: 'SLC',
+    city: 'Salt Lake City',
+    name: 'Salt Lake City International Airport'
+  },
+  {
+    id: 5,
+    code: 'ATL',
+    city: 'Atlanta',
+    name: 'Hartsfield-Jackson Atlanta International Airport'
   }
 ];
